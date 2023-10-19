f53b44697a77345e4abb5b5f0ce2834f7fb37c6c
joao-henrique10 <71076129+joao-henrique10@users.noreply.github.com>
2023-10-19 09:47:02 -0300

    Update data.js

--- data.js
+++ data.js
@@ -2,8 +2,10 @@ const abdul = { name: "Abdul Hannan", username: "hannanhunny01" };
 const adne = { name: "Adne Moretti Moreira", username: "AdneMoretti" };
 const andre = { name: "André Correa", username: "dartmol203" };
 const heitor = { name: "Heitor Barbosa", username: "heitormsb" };
+const joao = { name: "João Henrique Marques Calzavara", username: "joao-henrique10" };
 const leonardo = { name: "Leonardo Vitoriano", username: "leonardomilv3" };
 const lucas = { name: "Lucas Gabriel Sousa Camargo Paiva", username: "lucasgabriel-2" };
+const marcos = { name: "Marcos Vinícius de Deus", username: "Marcos574" };
 const puida = { name: "Guilherme Puida Moreira", username: "puida" };
 
 const users = [
@@ -11,8 +13,10 @@ const users = [
   adne,
   andre,
   heitor,
+  joao,
   leonardo,
   lucas,
+  marcos,
   puida,
 ];
 
@@ -23,6 +27,7 @@ const done = "done";
 const closed = "closed";
 
 const issues = [
+  { id: 87, package: "ruby-spy", authors: [joao], status: doing },
   { id: 85, package: "ruby-sys-filesystem", authors: [lucas], status: review },
   { id: 84, package: "ruby-xmlhash", authors: [abdul], status: doing },
   { id: 83, package: "ruby-solve", authors: [adne], status: doing },
@@ -36,6 +41,7 @@ const issues = [
   { id: 67, package: "python3-colored", authors: [adne], status: review },
   { id: 66, package: "python-flask-seeder", authors: [heitor], status: review },
   { id: 65, package: "python-flaky", authors: [andre], status: review },
+  { id: 63, package: "dnlib", authors: [joao, marcos], status: review },
   { id: 62, package: "flask-dance", authors: [puida, lucas], status: done },
 ];
 
